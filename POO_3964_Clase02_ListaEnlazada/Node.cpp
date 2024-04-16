d3ce3dc5829925409706c1171c8a3882541c8c2f
Diego Iván Cruz Ordiéres <diegocruzo@gmail.com>
2024-04-16 10:55:41 -0500

    Incluir listas enlazadas

---
+++ POO_3964_Clase02_ListaEnlazada/Node.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Diego Cruz on 19/03/2024.
+//
+
+#include "Node.h"
+
+Node::Node(int data){
+    this->data = data;
+    this->next = NULL;
+}
+
+int Node::getData() const {
+    return data;
+}
+void Node::setData(int data) {
+    Node::data = data;
+}
+
+Node* Node::getNext() const {
+    return next;
+}
+void Node::setNext(Node *next) {
+    Node::next = next;
+}
+
+
+
---
+++ POO_3964_Clase02_ListaEnlazada/main.cpp
@@ -0,0 +1,21 @@
+#include "LinkedList.h"
+
+int main() {
+    // Crear la instancia de la lista
+    LinkedList lista;
+    cout << "\nElementos de la lista:" << endl;
+    lista.Show();
+
+    // Adicionar nodos
+    lista.Insertlast(3);
+    lista.Insertlast(19);
+    lista.Insertlast(2024);
+    lista.Insertlast(365);
+
+    cout << "\nElementos de la lista:" << endl;
+    lista.Show();
+
+    cout << "\nCantidad de elementos en la lista: " << lista.getSize() << endl;
+
+    return 0;
+}
